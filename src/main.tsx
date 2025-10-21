e61d4741a4a6faa3babe3b132d37c6bd9e9c41a3
Carlos Perez <per.ckarlos@gmail.com>
2025-10-21 10:58:34 -0500

    adding configuration for pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/crew-assignment/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
